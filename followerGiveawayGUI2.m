6662bd13a3482b343d34dcf134acc43bd0a61c94
Sarmad Chaudhry <37321140+generaldamras@users.noreply.github.com>
2020-07-02 14:31:12 -0700

    Adding the GUI code
    followerGiveawayGUI2.m is a GUIDE-based GUI in MATLAB that allows you to dramatically select a random Twitch follower.

---
+++ followerGiveawayGUI2.m
@@ -0,0 +1,155 @@
+function varargout = followerGiveawayGUI2(varargin)
+% FOLLOWERGIVEAWAYGUI2 MATLAB code for followerGiveawayGUI2.fig
+%      FOLLOWERGIVEAWAYGUI2, by itself, creates a new FOLLOWERGIVEAWAYGUI2 or raises the existing
+%      singleton*.
+%
+%      H = FOLLOWERGIVEAWAYGUI2 returns the handle to a new FOLLOWERGIVEAWAYGUI2 or the handle to
+%      the existing singleton*.
+%
+%      FOLLOWERGIVEAWAYGUI2('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in FOLLOWERGIVEAWAYGUI2.M with the given input arguments.
+%
+%      FOLLOWERGIVEAWAYGUI2('Property','Value',...) creates a new FOLLOWERGIVEAWAYGUI2 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before followerGiveawayGUI2_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to followerGiveawayGUI2_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help followerGiveawayGUI2
+
+% Last Modified by GUIDE v2.5 20-Mar-2020 13:57:07
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @followerGiveawayGUI2_OpeningFcn, ...
+                   'gui_OutputFcn',  @followerGiveawayGUI2_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before followerGiveawayGUI2 is made visible.
+function followerGiveawayGUI2_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to followerGiveawayGUI2 (see VARARGIN)
+
+% Choose default command line output for followerGiveawayGUI2
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes followerGiveawayGUI2 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = followerGiveawayGUI2_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+filename = get(handles.edit1,'String');
+
+% Read .CSV
+fid = fopen(filename,'r');
+header = textscan(fgetl(fid),'%s','Delimiter',',');
+header = header{1};
+
+followerinfo = textscan(fid,'%s%s%s%s%s','Delimiter',',');
+followerinfo = followerinfo{1};
+
+handles.followers = regexprep(followerinfo(:,1),'"','');
+set(handles.uitable1, 'Data', handles.followers);
+guidata(hObject,handles)
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+numFollowers = length(handles.followers);
+winningNumber = randi(numFollowers);
+set(handles.edit2,'String',regexprep(handles.followers{winningNumber},'"',''));
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function pushbutton2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
